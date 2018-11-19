62990a2786b6da19bb2b93bf9014508d4affccab
wanghxm <wanghxm@yonyou.com>
2018-11-20 00:54:33 +0800

    ParallelUglifyPlugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 /**
  * Created by wanghongxiang on 2018/11/18.
  */
-import './index.html'
+// import './index.html'
+import extend from './module/extend'
+import fadeIn from './module/fadeIn'
+import getJSON from './module/getJSON'
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
+const os = require('os');
 const rootPath = path.resolve(__dirname, '.');     // 项目根目录
 const srcPath = path.join(rootPath, 'src');             // 开发源码目录
 // const env = process.env.NODE_ENV.trim();            // 当前环境
@@ -20,15 +21,26 @@ const commonPath = {
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    // mode: 'development',
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new webpack.HashedModuleIdsPlugin(), // 保持vendor文件不变
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html',
-            inject: true
+        // 主要用于大型生产导出
+        new ParallelUglifyPlugin({
+            workerCount: os.cpus().length - 1,//开启几个子进程去并发的执行压缩。默认是当前运行电脑的 CPU 核数减去1
+            uglifyJS: {
+                output: {
+                    beautify: false, //不需要格式化
+                    comments: true, //不保留注释
+                },
+                compress: {
+                    warnings: false, // 在UglifyJs删除没有用到的代码时不输出警告
+                    drop_console: true, // 删除所有的 `console` 语句，可以兼容ie浏览器
+                    collapse_vars: true, // 内嵌定义了但是只用到一次的变量
+                    reduce_vars: true, // 提取出出现多次但是没有定义成变量去引用的静态值
+                }
+            }
         })
     ],
     output: {
@@ -41,19 +53,5 @@ module.exports = {
         modules: [path.join(__dirname, './loaders'), 'node_modules']
     },
     module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [
-                    {loader: 'html-loader'},
-                    {
-                        loader: 'miniHtml-loader',
-                        options: {
-                            comments: false
-                        }
-                    }
-                ]
-            }
-        ]
     }
 }
