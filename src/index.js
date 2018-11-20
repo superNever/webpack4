468c13c4a3280c928776c6353916f7b5e98f2125
supernever <1021165525@qq.com>
2018-11-20 08:30:47 +0800

    dll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /**
  * Created by wanghongxiang on 2018/11/18.
  */
-import './index.html'
-import extend from './module/extend'
-import fadeIn from './module/fadeIn'
-import getJSON from './module/getJSON'
-import './index.css'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
